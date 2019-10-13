e83a5827cc115b53fef72e8fda20cf5fbdd53b2d
voldikss <dyzplus@gmail.com>
2019-10-13 10:18:12 +0800

    vim: update

--- scripts/coc_ext_release.sh
+++ scripts/coc_ext_release.sh
@@ -5,6 +5,6 @@ name=$(json name < package.json)
 echo "$name/$version"
 git commit -a -m "Release $version"
 git tag -a "$version" -m "Release $version"
+npm publish
 git push --tags
 git push
-npm publish
