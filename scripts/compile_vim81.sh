8136f0c2b9ffec2792181ea17f86e9d9fb0276c0
voldikss <dyzplus@gmail.com>
2019-10-26 14:57:59 +0800

    scripts: update compile_vim81.sh

--- scripts/compile_vim81.sh
+++ scripts/compile_vim81.sh
@@ -1,6 +1,6 @@
-#! /usr/local/bin/bash
+#! /usr/bin/env bash
 
-# cwd: vim-master
+git clone https://github.com/vim/vim --depth 1 && cd vim
 
 sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
 
